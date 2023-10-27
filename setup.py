22945aa14945cfc5d8c8811f6a0e09d6041a6911
GincyAG <gincy.m@beaglesecurity.com>
2023-10-27 15:07:00 +0530

    Add shiftcolumn aes function

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='aes',
+    version='0.1.0',
+    include_package_data=True,
+    packages=find_packages(exclude=['tests*']) + ['aes'],
+    zip_safe=False,
+    license='MIT',
+    description='AES',
+    long_description=open('README.txt').read(),
+    url='git@github.com:GincyAG/aes-.git',
+    author='Gincy',
+    install_requires=[
+        'tifffile==2021.11.2',
+        'numpy==1.23.5',
+        'selenium==3.141.0',
+        'validators',
+    ],
+)
---
+++ shiftrow.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<string.h>
+ 
+void main()
+
+{
+
+int i,j;
+
+char X[4][4],T[4][4];
+
+printf("Enter the text :");
+
+for(i=0;i<4;i++)
+
+{
+ 
+for(j=0;j<4;j++)
+ 
+{
+  
+X[i][j]=getchar();
+ 
+}
+
+}
+
+for(i=0;i<4;i++)
+
+{
+  
+for(j=0;j<4;j++)
+
+ {
+   
+ printf("%x ",X[i][j]);
+ 
+ }printf("\n");
+
+}
+
+for(i=1;i<4;i++)
+ 
+{
+ 
+ for(j=0;j<i;j++)
+  
+{
+ 
+ T[i][j]=X[i][j];
+  
+}
+  
+for(j=0;j<4;j++)
+  {
+  
+  if(j<(4-i))
+ 
+ {
+ 
+  X[i][j]=X[i][j+i];
+
+  }
+
+  if(j>=(4-i))
+  
+{
+
+  X[i][j]=T[i][j-(4-i)];
+  
+}
+  }
+
+ }
+ 
+for(i=0;i<4;i++)
+ 
+{
+ 
+ for(j=0;j<4;j++)
+ 
+ {
+
+    printf("%x ",X[i][j]);
+ 
+ }printf("\n");
+ 
+}
+ 
+}
